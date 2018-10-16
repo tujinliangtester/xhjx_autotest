e432fc34c998fd88d5eeb01c9b9336e22782211a
tujinliangtester <tujinliangtester@163.com>
2018-10-16 10:52:16 +0800

    pycharm

--- doTest/tmp1012.py
+++ doTest/tmp1012.py
@@ -1,6 +1,9 @@
 from initial import appium_init
 import time
 from selenium.webdriver.support.select import Select
+from tools import common
+
+
 driver=appium_init.driver
 #wo de
 el=driver.find_element_by_xpath('//*[@title="我的"]')
@@ -9,19 +12,9 @@ el.click()
 el=driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/div/div/div[2]/div[2]/div[1]')
 el.click()
 
-time.sleep(0.5)
-driver.switch_to.context('NATIVE_APP')
-print('switch to native')
-print(driver.current_context)
-dx=184/378
-dy=130/814
-x=driver.get_window_size()['width']*dx
-y=driver.get_window_size()['height']*dy
-driver.tap(x=x,y=y)
-
-driver.switch_to.context('WEBVIEW_com.tencent.mm:tools')
-print('switch to web')
-print(driver.current_context)
+dx=460/1080
+dy=600/1920
+common.click_native(dx=dx,dy=dy)
 
 
 el=driver.find_element_by_class_name('order-info')
