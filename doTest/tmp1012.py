37c96350d2974d691bf476912f5b6c923d6ac338
tujinliangtester <tujinliangtester@163.com>
2018-10-16 10:33:27 +0800

    pycharm

--- doTest/tmp1012.py
+++ doTest/tmp1012.py
@@ -3,21 +3,25 @@ import time
 from selenium.webdriver.support.select import Select
 driver=appium_init.driver
 #wo de
-el=driver.find_element_by_xpath('//*[@id="app"]/div/footer/div[2]/a[4]')
+el=driver.find_element_by_xpath('//*[@title="我的"]')
 el.click()
 
 el=driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/div/div/div[2]/div[2]/div[1]')
 el.click()
 
-el=driver.find_element_by_android_uiautomator()
 time.sleep(0.5)
-els=driver.find_elements_by_class_name('list_content')
-print(els[0])
-els[0].click()
-#
-# el=driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div[2]/div[1]/div/div/div/div[2]/div/div[2]/div/div')
-# # el=driver.find_element_by_class_name('list_content')
-# el.click()
+driver.switch_to.context('NATIVE_APP')
+print('switch to native')
+print(driver.current_context)
+dx=184/378
+dy=130/814
+x=driver.get_window_size()['width']*dx
+y=driver.get_window_size()['height']*dy
+driver.tap(x=x,y=y)
+
+driver.switch_to.context('WEBVIEW_com.tencent.mm:tools')
+print('switch to web')
+print(driver.current_context)
 
 
 el=driver.find_element_by_class_name('order-info')
